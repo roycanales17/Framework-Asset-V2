7309484b501bd5ae9302b9feda12d7161f8bd915
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2024-12-08 19:46:09 +0800

    Session handler

--- app/Session.php
+++ app/Session.php
@@ -1,15 +1,46 @@
 <?php
 
     namespace app;
+    use app\Handler\SessionHandler;
 
     class Session
     {
+        public static function setup(): void
+        {
+            # Configure
+            session_set_cookie_params([
+                'lifetime' => config('SESSION_LIFETIME'),
+                'path' => config('SESSION_PATH'),
+                'domain' => config('SESSION_DOMAIN', ''),
+                'secure' => (bool) config('SESSION_SECURE', false),
+                'httponly' => (bool) config('SESSION_HTTPONLY', true),
+            ]);
+
+            ini_set('session.gc_maxlifetime', config('SESSION_LIFETIME'));
+            ini_set('session.gc_probability', 1);
+            ini_set('session.gc_divisor', 100);
+
+            # Session Handler
+            session_set_save_handler(new \App\Handler\SessionHandler(), true);
+
+            # Register session close function
+            register_shutdown_function([new \App\Handler\SessionHandler(), 'session_close']);
+
+            # Start
+            self::start();
+        }
+
+        public static function started(): bool
+        {
+            return !(session_status() === PHP_SESSION_NONE);
+        }
+
         /**
          * Start the session if it hasn't already been started.
          */
         public static function start(): void
         {
-            if (session_status() === PHP_SESSION_NONE) {
+            if (!self::started()) {
                 session_start();
             }
         }
